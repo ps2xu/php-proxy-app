764ac2f20f4d09afc5d5337a2fb7e67bc3166d1e
ps2xu <ps2xu@163.com>
2020-09-25 12:29:57 +0800

    Update php-ro-app.sh

--- php-ro-app.sh
+++ php-ro-app.sh
@@ -5,9 +5,6 @@ read -p "请设置你的容器内存大小(默认256):" ramsize
 if [ -z "$ramsize" ];then
 	ramsize=256
 fi
-rm -rf *
-git clone https://github.com/ps2xu/php-proxy-app.git
-cd php-proxy-app
 ibmcloud target --cf
 ibmcloud cf push
 echo "Rebuild completed."
